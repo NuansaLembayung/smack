3320e537117a7f7894201523524c283ea06d2477
Nuansa Lembayung <nuansa@icloud.com>
2017-08-20 00:35:10 +0800

    add auth service

--- smack/smack/Utilities/Constants.swift
+++ smack/smack/Utilities/Constants.swift
@@ -8,8 +8,20 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success:Bool) -> ()
+
+//URL
+let BASE_URL = "https://nuansa-smack.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
+
 //Segues
 let TO_LOGIN = "LoginVCSegue"
 let TO_REGISTER = "RegisterVCSegue"
 let TO_AVATAR = "AvatarVCSegue"
 let BACK_CHANNEL = "ChannelVCUnwind"
+
+//User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
